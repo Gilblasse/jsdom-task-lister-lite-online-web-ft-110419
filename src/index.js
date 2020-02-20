5d1b2244ae6c78f25966c30af0d56ad887af7522
Nethelbert <gilblasse@gmail.com>
2020-02-20 17:32:03 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 const form = document.querySelector("#create-task-form")
-document.addEventListener("DOMContentLoaded", () => {
-  
+document.addEventListener("DOMContentLoaded", (e) => {
+  stopForm (e);
 });
+
+function stopForm (e){
+  e.preventDefault
+}
