f26c1a53ec1bd1fb3cd75054803d14913cf6087a
Nethelbert <gilblasse@gmail.com>
2020-02-20 18:33:07 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -16,6 +16,6 @@ form.addEventListener("submit", (e) => {
 
 document.addEventListener('click',function(e){
     if (e.target.tagName == "BUTTON") {
-      e.target.parentElement.remove()
+      e.target.parentElement.remove();
     }
 })
