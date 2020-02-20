7a0c7f9da9bc636f496a0cc08979537008635afe
Nethelbert <gilblasse@gmail.com>
2020-02-20 18:02:03 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -10,5 +10,6 @@ form.addEventListener("submit", (e) => {
   let task = userInput.value;
   let liTag = document.createElement('li');
   liTag.innerHTML = `${task} <button>&#88;<button>`
+  ulTag.appendChild(liTag)
   e.preventDefault();
 });
