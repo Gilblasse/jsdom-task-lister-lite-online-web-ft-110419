1d68a3dab2851d365d54e30815dfdc403f9b978a
Nethelbert <gilblasse@gmail.com>
2020-02-20 17:56:35 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const form = document.querySelector("#create-task-form");
 const userInput = document.querySelector("#new-task-description");
+const ulTag = document.querySelector("#list #tasks");
 
 document.addEventListener("DOMContentLoaded", () => {
 
