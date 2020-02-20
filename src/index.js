c19ce7ade015bbf23b9bd69df8e772b19d6eaa7a
Nethelbert <gilblasse@gmail.com>
2020-02-20 17:46:02 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 const submitButton = document.querySelector("#create-task-form #submit");
 document.addEventListener("DOMContentLoaded", () => {
-  addToList();
+
 });
 
-function addToList(){
-  submitButton.addEventListener("submit", (e) => {
-    console.log(e);
-    e.preventDefault;
-  });
-}
+submitButton.addEventListener("submit", (e) => {
+  console.log(e);
+  e.preventDefault;
+});
